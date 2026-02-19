b5ba0bb81e68452d4fbdf785404ccc2dfc284a2a
vd <mrafighalibf@gmail.com>
2026-02-20 00:31:23 +0700

    chore: initialize project

---
+++ src/main/java/id/ac/ui/cs/advprog/yomubacaandankuis/YomuBacaanDanKuisApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomubacaandankuis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomuBacaanDanKuisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YomuBacaanDanKuisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/yomubacaandankuis/YomuBacaanDanKuisApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomubacaandankuis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YomuBacaanDanKuisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
